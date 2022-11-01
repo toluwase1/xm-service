ba7ab1996e9a47b65007eebaec944776382ca24d
toluwase <toluwasethomas1@gmail.com>
2022-11-01 11:09:31 +0100

    modelling

---
+++ company-service/models/company.go
@@ -0,0 +1,21 @@
+package models
+
+import "github.com/google/uuid"
+
+type Company struct {
+	Id                uuid.UUID
+	Name              string `json:"name"  gorm:"unique;default:null" binding:"required"`
+	Description       string `json:"description,omitempty"`
+	AmountOfEmployees int    `json:"amount_of_employees"  gorm:"unique" binding:"required"`
+	IsRegistered      bool   `json:"is_registered" gorm:"unique;not null" binding:"required,is_registered"`
+	Type              string `json:"type" binding:"required"`
+}
+
+/*
+• ID (uuid) required
+• Name (15 characters) required - unique
+• Description (3000 characters) optional
+• Amount of Employees (int) required
+• Registered (boolean) required
+• Type (Corporations | NonProfit | Cooperative | Sole Proprietorship) required
+*/
